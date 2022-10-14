dfc51e6a783ef3576d24047d2e172f0196524c78
VickyBondarenko <quikyv@gmail.com>
2022-10-14 21:19:02 +0200

    Added GitHub link

--- src/App.js
+++ src/App.js
@@ -5,6 +5,16 @@ function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
+      <footer>
+        This progect was coded by Vicky Bondarenko and is{" "}
+        <a
+          href="https://github.com/VickyBondarenko/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
     </div>
   );
 }
