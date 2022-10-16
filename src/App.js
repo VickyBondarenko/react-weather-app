de8d1d0d65aa7290c925f270a81d8837ece931cb
VickyBondarenko <quikyv@gmail.com>
2022-10-16 13:14:39 +0200

    Added basic css

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
 import React from "react";
 import "./App.css";
+import Weather from "./Weather";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This progect was coded by Vicky Bondarenko and is{" "}
-        <a
-          href="https://github.com/VickyBondarenko/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <Weather />
+        <footer>
+          This progect was coded by Vicky Bondarenko and is{" "}
+          <a
+            href="https://github.com/VickyBondarenko/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
-
-export default App;
---
+++ src/Weather.js
@@ -0,0 +1,84 @@
+import React from "react";
+import "./Weather.css";
+
+export default function Weather() {
+  return (
+    <div className="Weather">
+      <form className="enter-city" id="enter-city">
+        <input
+          type="search"
+          id="city-input"
+          placeholder="Enter city "
+          autoFocus="on"
+        />
+        <input type="submit" className="btn btn-primary" value="Change" />
+        <button type="button" className="btn btn-info">
+          Current
+        </button>
+      </form>
+      <div className="row">
+        <div className="col-4">
+          <ul>
+            <li>
+              <span className="city" id="city">
+                {" "}
+                Dnipro
+              </span>
+            </li>
+            <li>
+              <div className="current-weather" id="current-weather">
+                {" "}
+                Cloud
+              </div>
+              <span className="conditions">
+                Humidity: <span id="humidity"> 20</span> %
+                <br />
+                Winds: <span id="wind"> 3</span> km/h
+              </span>
+            </li>
+          </ul>
+        </div>
+        <div className="col-4 description">
+          <ul>
+            <li>
+              <div className="day-temp">
+                <span className="temperature" id="temp">
+                  25
+                </span>
+                <span className="units">
+                  <a href="#" id="celsius-link" className="active">
+                    °C
+                  </a>{" "}
+                  |
+                  <a href="#" id="fahrenheit-link">
+                    °F
+                  </a>
+                </span>
+              </div>
+            </li>
+            <li>
+              <div className="day" id="dayTime">
+                {" "}
+                Saturday
+              </div>
+              <div className="date" id="date">
+                15.10.2022
+              </div>
+            </li>
+          </ul>
+        </div>
+        <div className="col-4">
+          <span>
+            <img
+              src={" "}
+              alt="sun"
+              className="icon-today"
+              id="icon"
+              weight="100"
+            />
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+}
